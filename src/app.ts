8127c834be626ea39ecb5ea280cffa30dcbf38da
damanjot6767 <damanjot6767@gmail.com>
2024-02-07 13:29:30 -0500

    fix socket error

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ const io = new Server(httpServer, {
 
 app.set("io", io); // using set method to mount the `io` instance on the app to avoid usage of `global`
 
-
+console.log('28',process.env.CORS_ORIGIN)
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
