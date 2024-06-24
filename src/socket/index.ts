f6d80da4f810f9e68c99f57be13568aa2723fa91
damanjot6767 <damanjot6767@gmail.com>
2024-06-24 14:39:28 +0530

    fix some socket bugs

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,99 +1,102 @@
 import cookie from "cookie";
 import jwt from "jsonwebtoken";
-import WebSocket, { WebSocketServer} from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 import { ChatEventEnum } from "../constants";
 import { Request } from "express";
 import { UserModel } from "../models/index";
-import { ApiError } from "../utils/api-error";;
+import { ApiError } from "../utils/api-error";
 import { ChatResponseDto, CreateChatDto } from "../controllers/chats/dto";
+import { getChatsByUserId } from "../models/chat.model";
 
+const conversationsObject = {};
 
-const conversationsObject = {}
+const authorizeEvent = async (socket, data) => {
+  try {
+    let token = data?.token; // get the accessToken
 
-const authorizeEvent = async(socket: any, data: any)=>{
-           try {
-            let token:any = data?.token; // get the accessToken
+    if (!token) {
+      // If there is no access token in cookies. Check inside the handshake auth
+      token = socket.handshake.query?.token;
+    }
 
-            if (!token) {
-                // If there is no access token in cookies. Check inside the handshake auth
-                token = socket.handshake.query?.token
-            }
+    if (!token) {
+      // Token is required for the socket to work
+      throw new ApiError(401, "Unauthorized handshake. Token is missing");
+    }
 
-            if (!token) {
-                // Token is required for the socket to work
-                throw new ApiError(401, "Un-authorized handshake. Token is missing");
-            }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET); // decode the token
 
-            const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET); // decode the token
+    const user = await UserModel.findById(decodedToken).select("-password -refreshToken");
 
-            const user = await UserModel.findById(decodedToken).select(
-                "-password -refreshToken "
-            );
+    // retrieve the user
+    if (!user) {
+      throw new ApiError(401, "Unauthorized handshake. Token is invalid");
+    }
 
-            // retrieve the user
-            if (!user) {
-                throw new ApiError(401, "Un-authorized handshake. Token is invalid");
-            }
-            socket.user = user; // mount te user object to the socket
-           } catch (error) {
-             socket
-           }
-}
+    // create chat socket events
+    const chatsByUserId = await getChatsByUserId(user._id);
 
-const initializeSocketIO = (io: WebSocketServer) => {
-    return io.on("connection", async (socket: WebSocketServer) => {
-
-            socket.on('message', async(res)=>{
-                try {
-                    const data = JSON.parse(res);
-                    if(data.event===ChatEventEnum.CONNECTED_EVENT){
-                        await authorizeEvent(socket,data)
-                        console.log('connected','new user connected ');
-                    }
-
-                    else if(data.event===ChatEventEnum.JOIN_CHAT_EVENT){
-                        if(conversationsObject[data.chatId]){
-                            const sockets = [...conversationsObject[data.chatId],socket]
-                            conversationsObject[data.chatId]=sockets
-
-                        }
-                        else{
-                            conversationsObject[data.chatId]=[socket];
-                        }
-                    }
-
-                    else if(data.event===ChatEventEnum.MESSAGE_RECEIVED_EVENT){
-                        conversationsObject[data.chatId]?.map((item)=>{
-                            if(item!==socket){
-                                socket.send(data);
-                            }
-                        })
-                    }
-                    
-                    else if(data.event===ChatEventEnum.TYPING_EVENT){
-                        console.log('tyoing', conversationsObject[data.chatId])
-                        conversationsObject[data.chatId]?.map((item)=>{
-                            if(item!==socket){
-                                socket.send(data);
-                            }
-                        })
-                    }
-                   
-                } catch (error) {
-                    socket.on("error",()=>{
-                        console.log("soceket error while receiving event")
-                    })
-                }
-            });
-
-            socket.on("error",()=>{
-                console.log("soceket error while connecting socket")
-            })
-
-            console.log('new connection')
-
+    chatsByUserId?.forEach((ele) => {
+      if (conversationsObject[ele._id]) {
+        const sockets = [...conversationsObject[ele._id], socket];
+        conversationsObject[ele._id] = sockets;
+      } else {
+        conversationsObject[ele._id] = [socket];
+      }
     });
+  } catch (error) {
+    console.log('Something went wrong', error);
+  }
 };
 
+const initializeSocketIO = (io) => {
+  io.on("connection", async (socket) => {
+    console.log('New connection');
+
+    socket.on('message', async (res) => {
+      try {
+        const data = JSON.parse(res);
+        if (data.event === ChatEventEnum.CONNECTED_EVENT) {
+          await authorizeEvent(socket, data);
+          console.log('Connected: new user connected', io.clients.size);
+        } else if (data.event === ChatEventEnum.JOIN_CHAT_EVENT) {
+          if (conversationsObject[data.chatId]) {
+            const sockets = [...conversationsObject[data.chatId], socket];
+            conversationsObject[data.chatId] = sockets;
+          } else {
+            conversationsObject[data.chatId] = [socket];
+          }
+        } else if (data.event === ChatEventEnum.MESSAGE_RECEIVED_EVENT) {
+          conversationsObject[data.chatId]?.forEach((item) => {
+            if (item !== socket) {
+              item.send(JSON.stringify(data));
+            }
+          });
+        } else if (data.event === ChatEventEnum.TYPING_EVENT) {
+          conversationsObject[data.chatId]?.forEach((item) => {
+            if (item !== socket) {
+              item.send(JSON.stringify(data));
+            }
+          });
+        }
+      } catch (error) {
+        console.log('Error while handling message', error);
+      }
+    });
+
+    socket.on("error", (error) => {
+      console.log("Socket error:", error);
+    });
+
+    socket.on('close', (code, reason) => {
+      console.log('Client disconnected', { code, reason });
+      for (let chatId in conversationsObject) {
+        let sockets = conversationsObject[chatId];
+        sockets = sockets?.filter((item) => item !== socket);
+        conversationsObject[chatId] = sockets;
+      }
+    });
+  });
+};
 
 export { initializeSocketIO };
